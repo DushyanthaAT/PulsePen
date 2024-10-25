30451226193e7d07e79ba358abe34221b6ed80ec
DushyanthaAT <dushyantha721@gmail.com>
2024-10-25 20:01:16 +0530

    added pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,23 @@
 import React from "react";
-import { BrowserRoter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import Project from "./pages/Project";
 
 export default function App() {
-  return <div className="text-3xl text-red-600">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
