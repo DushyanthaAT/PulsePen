1de13c8f5edee57177632bdfeb4ba6ac8b8485c5
DushyanthaAT <dushyantha721@gmail.com>
2024-10-25 19:47:01 +0530

    Initial commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { BrowserRoter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return <div className="text-3xl text-red-600">App</div>;
+}
