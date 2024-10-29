6f01ac1a169406d6253f462929cf7c06e9060642
DushyanthaAT <dushyantha721@gmail.com>
2024-10-30 02:35:25 +0530

    update header compoent with user pro pic

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "../assets/LOGO.png";
 import { CiSearch } from "react-icons/ci";
 import { FaMoon } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const path = useLocation().pathname;
-
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Navbar className="border-b-2">
       {/* Logo Section */}
@@ -25,11 +26,38 @@ const Header = () => {
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link>
-          <Button gradientDuoTone="purpleToBlue" outline>
-            Signin
-          </Button>
-        </Link>
+
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt={currentUser.username}
+                img={currentUser.ProfilePicture}
+                rounded
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="blovk text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign Out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Signin
+            </Button>
+          </Link>
+        )}
+
         <Navbar.Toggle />
       </div>
 
