d459993a1fa9fd1bf008bad52f9c87eb1376c3e0
DushyanthaAT <dushyantha721@gmail.com>
2024-11-01 20:29:39 +0530

    make dashboard private

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 const Dashboard = () => {
-  return <div>Dashbord</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParms = new URLSearchParams(location.search);
+    const tabFromUrl = urlParms.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <div>
+      {/* sideNav */}
+      <DashSidebar />
+      {/* RightSide */}
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 };
 
 export default Dashboard;
