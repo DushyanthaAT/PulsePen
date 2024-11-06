be2566be7649ea692729ef0d45e44803d9b6fd32
DushyanthaAT <dushyantha721@gmail.com>
2024-11-06 09:12:36 +0530

    complete user image upload

--- Client/src/components/DashProfile.jsx
+++ Client/src/components/DashProfile.jsx
@@ -1,19 +1,108 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Button, TextInput } from "flowbite-react";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from "../firebase";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 
 const DashProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(currentUser?.ProfilePicture || null);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+
+  const filePickerRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  const uploadImage = async () => {
+    setImageFileUploadError(null);
+    if (!imageFile) return;
+
+    try {
+      const storage = getStorage(app);
+      const fileName = `${new Date().getTime()}_${imageFile.name}`;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, imageFile);
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageFileUploadProgress(progress.toFixed(0));
+        },
+        (error) => {
+          setImageFileUploadError('Could not upload image (File must be less than 2MB)');
+        },
+        async () => {
+          const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+          setImageFileUrl(downloadURL);
+        }
+      );
+    } catch (error) {
+      setImageFileUploadError('Unexpected error occurred during upload.');
+      setImageFile(null);
+      setImageFileUrl(null);
+    }
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
   return (
-    <div className="max-w-lg mx-auto p-3 w-full h-screen w-screen">
-      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+    <div className="max-w-lg mx-auto p-3 w-full h-screen">
+      <h1 className="my-7 text-center font-semibold text-3xl dark:text-white">
+        Profile
+      </h1>
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full relative"
+          onClick={() => filePickerRef.current.click()}
+        >
+           {imageFileUploadProgress && (
+            <CircularProgressbar
+              value={imageFileUploadProgress || 0}
+              text={`${imageFileUploadProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: '100%',
+                  height: '100%',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(140, 198, 63, ${
+                    imageFileUploadProgress / 100
+                  })`,
+                },
+              }}
+            />
+          )}  
           <img
-            src={currentUser.ProfilePicture}
+            src={imageFileUrl || currentUser.ProfilePicture}
             alt="user"
-            className="rounded-full w-full h-full border-8 object-cover border-[lighgray]"
-            defaultValue={currentUser.ProfilePicture}
+            className={`rounded-full w-full h-full object-cover border-8 border-lightgray ${imageFileUploadProgress && imageFileUploadProgress < 100 && 'opacity-60'}`}
           />
         </div>
         <TextInput
@@ -22,19 +111,26 @@ const DashProfile = () => {
           placeholder="Username"
           defaultValue={currentUser.username}
         />
-
         <TextInput
           type="email"
           id="email"
           placeholder="Email"
           defaultValue={currentUser.email}
         />
-
         <TextInput type="password" id="password" placeholder="Password" />
-
         <Button type="submit" color="dark" className="dark:bg-slate-400">
           Update
         </Button>
+        {imageFileUploadProgress && (
+          <div className="text-center mt-2">
+            Upload Progress: {imageFileUploadProgress}%
+          </div>
+        )}
+        {imageFileUploadError && (
+          <div className="text-red-500 text-center mt-2">
+            {imageFileUploadError}
+          </div>
+        )}
         <div className="text-red-500 flex justify-between mt-5">
           <span className="cursor-pointer">Delete Account</span>
           <span className="cursor-pointer">Sign Out</span>
--- Client/src/firebase.js
+++ Client/src/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-blog-e7f45.firebaseapp.com",
-  projectId: "mern-blog-e7f45",
-  storageBucket: "mern-blog-e7f45.appspot.com",
-  messagingSenderId: "669660739938",
-  appId: "1:669660739938:web:7a86db09e0a4e84215943b",
+  authDomain: "fir-test-c691b.firebaseapp.com",
+  projectId: "fir-test-c691b",
+  storageBucket: "fir-test-c691b.appspot.com",
+  messagingSenderId: "426646652568",
+  appId: "1:426646652568:web:f85a3d7b7c6fa62d9cf2ed",
+  measurementId: "G-NEZBPCTHEN"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
