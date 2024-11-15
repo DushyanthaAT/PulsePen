d32fa3d66920ca178b9dbf2a6f451e8a15b72cc1
DushyanthaAT <dushyantha721@gmail.com>
2024-11-16 00:52:23 +0530

    add comment section to the post page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -13,6 +13,7 @@ import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
+import ScrollToTop from "./components/ScrollToTOp";
 
 function App() {
   const location = useLocation();
@@ -45,6 +46,7 @@ function App() {
 export default function AppWrapper() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   );
