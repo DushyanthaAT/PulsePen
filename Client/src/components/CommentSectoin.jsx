ba6f3effc5a7a9f2b76dd1400f6b7df9ace8cac3
DushyanthaAT <dushyantha721@gmail.com>
2024-11-16 18:26:45 +0530

    add like funtionality to the comments

--- Client/src/components/CommentSectoin.jsx
+++ Client/src/components/CommentSectoin.jsx
@@ -1,7 +1,7 @@
 import { Alert, Button, Modal, TextInput, Textarea } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Comment from "./Comment";
 
 export default function CommentSection({ postId }) {
@@ -11,6 +11,9 @@ export default function CommentSection({ postId }) {
   const [comments, setComments] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [commentToDelete, setCommentToDelete] = useState(null);
+
+  const navigate = useNavigate();
+
   console.log(comment);
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -55,9 +58,37 @@ export default function CommentSection({ postId }) {
     getComments();
   }, [postId]);
 
-  const handleLike = async () => {};
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: "PUT",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   const handleEdit = async () => {};
   const handleDelete = async () => {};
+
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
