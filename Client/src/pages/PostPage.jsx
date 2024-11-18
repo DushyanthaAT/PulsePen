4645f5e841e84bad8e5329effe3445f24ff208bb
DushyanthaAT <dushyantha721@gmail.com>
2024-11-18 22:43:04 +0530

    Update PostPage.jsx

--- Client/src/pages/PostPage.jsx
+++ Client/src/pages/PostPage.jsx
@@ -86,9 +86,6 @@ export default function PostPage() {
           className="p-3 max-w-2xl mx-auto w-full post-content"
           dangerouslySetInnerHTML={{ __html: post && post.content }}
         ></div>
-        <div className="max-w-4xl mx-auto">
-          <CallToAction />
-        </div>
         <CommentSectoin postId={post._id} />
         <div className="flex flex-col justify-center items-center mb-5">
           <h1 className="text-xl mt-5">Recent Articals</h1>
