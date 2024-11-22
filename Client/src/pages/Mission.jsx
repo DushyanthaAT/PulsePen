59f7fb959f5f94d3e1e5f092f3d1a93bf8a7dd8f
DushyanthaAT <dushyantha721@gmail.com>
2024-11-23 00:01:21 +0530

    Create Mission.jsx

---
+++ Client/src/pages/Mission.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Mission = () => {
+  return (
+    <div className="flex flex-col w-full bg-white  dark:bg-gray-900 justify-center items-center dark:text-white">
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="max-w-2xl mx-auto p-3 text-center">
+          <div>
+            <div className="text-md flex flex-col gap-6">
+              <h2 className="text-xl font-bold">Our Mission</h2>
+              <p>
+                To empower individuals to express themselves, connect with
+                others, and spark meaningful conversations through the written
+                word. Whether you're sharing a life experience, exploring a
+                creative idea, or diving into a trending topic, PulsePen is the
+                place to let your voice be heard.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
