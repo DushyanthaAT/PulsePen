27119db9ac3fad3d51750af39965cdb6efad3f13
DushyanthaAT <dushyantha721@gmail.com>
2024-11-23 23:41:32 +0530

    change base url

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://pulse-pen.vercel.app/",
         secure: false,
       },
     },
