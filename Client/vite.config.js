6f8883118ddb406c2d5d15426d4ea26224ef5353
DushyanthaAT <dushyantha721@gmail.com>
2024-11-24 11:55:12 +0530

    change  vite

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        secure: false,
-      },
-    },
-  },
   plugins: [react()],
-});
+  build:{
+    outDir: "build"
+  }
+})
